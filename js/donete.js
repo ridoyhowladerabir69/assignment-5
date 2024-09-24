a6d3e03267a7c9f3eb6121f8db18a3e30f4cb314
ridoyhowladerabir69 <ridoyhowladerabir@gmail.com>
2024-09-24 14:04:00 +0600

    add to the donate section

--- js/donete.js
+++ js/donete.js
@@ -3,37 +3,131 @@ document.getElementById('donate-btn1')
         event.preventDefault();
 
         const donateMoney = getInputFieldValueById('noakhali-donate');
-        if(isNaN(donateMoney)){
+        if(isNaN(donateMoney) || donateMoney === ''){
             alert('Please Enter a Amount')
             return;
         }
 
         if(donateMoney){
+            const mainBalance = getTextFieldValueById('main-balance');
+            if (donateMoney > mainBalance) {
+                alert('Your balance is low');
+                return;
+            }
             const donateBalance = getTextFieldValueById('donate-balance1');
             const newDonateBalance = donateBalance + donateMoney;
-
             document.getElementById('donate-balance1')
                 .innerText = newDonateBalance;
 
-            if(donateMoney){
-                const mainBalance = getTextFieldValueById('main-balance');
-                if(donateMoney > mainBalance){
-                    alert('Your balance is low');
-                    return;
-                }
-                const newMainBalance = mainBalance - donateMoney;
-        
-                document.getElementById('main-balance')
-                    .innerText = newMainBalance;
-            }
+            const newMainBalance = mainBalance - donateMoney;
+            document.getElementById('main-balance')
+                .innerText = newMainBalance;
             
-            const div = document.createElement('div');
-            div.classList.add('text-lime-500');
-            div.innerHTML = `<p>${donateMoney} Taka is Donated for Flood Relief in Feni,Bangladesh</p>`
+            const currentDate = new Date();
+            const date = currentDate.toLocaleDateString();
+            const time = currentDate.toLocaleTimeString();
 
+            const div = document.createElement('div');
+            div.classList.add('border', 'rounded-lg', 'py-2', 'px-2', 'text-xl', 'font-bold', 'mb-3');
+            div.innerHTML = `<p>${donateMoney} Taka is Donated for Flood Relief in Noakhali, Bangladesh</p>
+            <small class="text-gray-500">Date: ${date} at ${time}</small>`
+            
             document.getElementById('history-container')
                 .appendChild(div);
+
+                // alert
+            alert('You Have Donated for Humankind')
         }
 
-        
+    })
+
+    // function step 2
+
+    document.getElementById('donate-btn2')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+
+        const donateMoney = getInputFieldValueById('feni-donate');
+        if(isNaN(donateMoney) || donateMoney === ''){
+            alert('Please Enter a Amount')
+            return;
+        }
+
+        if(donateMoney){
+            const mainBalance = getTextFieldValueById('main-balance');
+            if (donateMoney > mainBalance) {
+                alert('Your balance is low');
+                return;
+            }
+            const donateBalance = getTextFieldValueById('donate-balance2');
+            const newDonateBalance = donateBalance + donateMoney;
+            document.getElementById('donate-balance2')
+                .innerText = newDonateBalance;
+
+            const newMainBalance = mainBalance - donateMoney;
+            document.getElementById('main-balance')
+                .innerText = newMainBalance;
+            
+            const currentDate = new Date();
+            const date = currentDate.toLocaleDateString();
+            const time = currentDate.toLocaleTimeString();
+
+            const div = document.createElement('div');
+            div.classList.add('border', 'rounded-lg', 'py-2', 'px-2', 'text-xl', 'font-bold', 'mb-3');
+            div.innerHTML = `<p>${donateMoney} Taka is Donated for Flood Relief in Feni, Bangladesh</p>
+            <small class="text-gray-500">Date: ${date} at ${time}</small>`
+            
+            document.getElementById('history-container')
+                .appendChild(div);
+
+                // alert
+            alert('You Have Donated for Humankind')
+        }
+
+    })
+
+
+    // function part 3
+
+    document.getElementById('donate-btn3')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+
+        const donateMoney = getInputFieldValueById('quota-donate');
+        if(isNaN(donateMoney) || donateMoney === ''){
+            alert('Please Enter a Amount')
+            return;
+        }
+
+        if(donateMoney){
+            const mainBalance = getTextFieldValueById('main-balance');
+            if (donateMoney > mainBalance) {
+                alert('Your balance is low');
+                return;
+            }
+            const donateBalance = getTextFieldValueById('donate-balance3');
+            const newDonateBalance = donateBalance + donateMoney;
+            document.getElementById('donate-balance3')
+                .innerText = newDonateBalance;
+
+            const newMainBalance = mainBalance - donateMoney;
+            document.getElementById('main-balance')
+                .innerText = newMainBalance;
+            
+            const currentDate = new Date();
+            const date = currentDate.toLocaleDateString();
+            const time = currentDate.toLocaleTimeString();
+
+            const div = document.createElement('div');
+            div.classList.add('border', 'rounded-lg', 'py-2', 'px-2', 'text-xl', 'font-bold', 'mb-3');
+            div.innerHTML = `<p>${donateMoney} Taka is Donated for Flood Relief in Quota, Bangladesh</p>
+            <small class="text-gray-500">Date: ${date} at ${time}</small>`
+            
+            document.getElementById('history-container')
+                .appendChild(div);
+
+                // alert
+            alert('You Have Donated for Humankind')
+        }
+
     })
